633fe69bc643c55eec0550cbd6091583ef0e30b0
Thieres Tembra <tdt@mytdt.com.br>
2021-03-23 18:42:33 -0300

    Updated gitigone and rename card folder to lowercase

---
+++ src/components/card/Info.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CSSTransition } from 'react-transition-group';
+
+import './Info.scss';
+
+/**
+ * Information component that renders inside a card.
+ *
+ * @param  {Array}        props.infos Array of informations to render
+ * @param  {Boolean}      props.show  Whether is to show the element
+ * @return {ReactElement}             The markup to render
+ */
+const Info = ({ infos, show }) => {
+  const nodeRef = React.useRef(null);
+  const numberFormat = new Intl.NumberFormat('pt-BR');
+
+  return (
+    <CSSTransition
+      nodeRef={ nodeRef }
+      in={ show }
+      appear={ show }
+      timeout={ {
+        appear: 300,
+        enter: 300,
+        exit: 125,
+      } }
+      mountOnEnter
+      unmountOnExit
+    >
+      <div ref={ nodeRef } className="card-info">
+        {
+          infos.map((singleInfo, index) => {
+            const left = (index + 1) % 2 !== 0;
+            const className = left ? 'info-left' : 'info-right';
+            const value = numberFormat.format(singleInfo.value);
+
+            return (
+              <div key={ index } className={ className }>
+                <span className="info-value" title={ value }>
+                  { value }
+                </span>
+                <span className="info-name">{ singleInfo.name }</span>
+              </div>
+            );
+          })
+        }
+      </div>
+    </CSSTransition>
+  );
+};
+
+Info.propTypes = {
+  infos: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  show: PropTypes.bool.isRequired,
+};
+
+export default Info;
